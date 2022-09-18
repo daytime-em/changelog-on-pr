c688b3a138e41d9497a748ebf85f8f5cfe8ef213
Emily Dixon <edixon@mux.com>
2022-09-18 07:11:40 -0700

    Added Basic Changelog generation (#1)

--- notes.js
+++ notes.js
@@ -1,7 +1,11 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
 
-function prNumber() {
+const octokit = new github.getOctokit(core.getInput('token'))
+const owner = github.context.payload.repository.owner.login
+const repo = github.context.payload.repository.name
+
+function getPullNumber() {
   var manualNumber = core.getInput('pull_number')
   if (manualNumber) {
     return manualNumber
@@ -10,13 +14,57 @@ function prNumber() {
   }
 }
 
-async function fetchPr() {
+function createChangelog(commitMessages) {
+  // it would be cool would be to sort into different headings by PR label (pr number from the #x at the end)
+  const header = "## Improvements"
+
+  var body = header + "\n\n"
+  commitMessages.map(msg => { return msg.split('\n')[0] })
+    .forEach(msg => { body += "* " + msg + "\n" })
+
+  return body
 }
 
 async function main() {
-  console.log("Working on PR number " + prNumber())
+  let pullNumber = getPullNumber()
+  console.log(">release-notes-on-pr: Working on PR number " + pullNumber)
+
+  let commits = await octokit.paginate(
+    octokit.rest.pulls.listCommits, {
+    owner,
+    repo,
+    pull_number: pullNumber
+  })
+  let commitMessages = commits.map(element => { return element.commit.message })
+  let changelog = createChangelog(commitMessages)
+
+  console.log("Adding Changelog:\n" + changelog)
+
+  // Append the changelog to what's already in there
+  let pr = await octokit.rest.pulls.get({
+    owner,
+    repo,
+    pull_number: pullNumber
+  })
+  var body
+  if (pr.body) {
+    body = pr.body + "\n\n" + changelog
+  } else {
+    body = changelog
+  }
+
+  await octokit.rest.pulls.update({
+    owner,
+    repo,
+    pull_number: pullNumber,
+    body
+  })
 }
 
-main().catch(err => { core.setFailed(err.message) })
-
-console.log("Done!")
+main()
+  .catch(err => {
+    console.log("Failed with error")
+    console.log(err)
+    core.setFailed(err.message)
+  })
+  .then(() => { console.log("Done! ") })
