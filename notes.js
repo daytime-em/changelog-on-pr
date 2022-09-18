c9b7d7c0b0a81aebd695cc149c84a500bbea395d
Emily Dixon <edixon@mux.com>
2022-09-17 20:38:24 -0700

    Parsed data

--- notes.js
+++ notes.js
@@ -39,9 +39,7 @@ async function main() {
     repo,
     pull_number : pullNumber 
   })
-  let l = JSON.parse(JSON.stringify(commits))
-  console.log('Parsed the object ' + l)
-  console.log('Parsed data is ' + l.data)
+  console.log('Parsed data is ' + commits.data)
   console.log('Hey I downloaded a commit! ' + JSON.stringify(commits))
 
   // Fetch PR Commits
